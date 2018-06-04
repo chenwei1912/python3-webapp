247c8d3a68e201520e795e80566db020d0f6bc0f
Ethan <Ethan@github.com>
2018-06-04 11:03:03 +0800

    slightly modify app.py

--- www/app.py
+++ www/app.py
@@ -12,15 +12,16 @@ from aiohttp import web
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
 
-@asyncio.coroutine
-def init(loop):
+#@asyncio.coroutine
+async def init(loop):
     app = web.Application(loop=loop)
 
     # 添加get响应方法
     app.router.add_route('GET', '/', index)
 
     # 用异步协程方法创建一个网络服务
-    srv = yield from loop.create_server(app.make_handler(), '0.0.0.0', 9000)
+    #srv = yield from loop.create_server(app.make_handler(), '0.0.0.0', 9000)
+    srv = await loop.create_server(app.make_handler(), '0.0.0.0', 9000)
 
     logging.info('server started at http://0.0.0.0:9000...')
     return srv
